d414105ec96d9e5b953bb31d8421c444751fd30e
PatrickCodeing <117385304+PatrickCodeing@users.noreply.github.com>
2023-07-04 17:16:52 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ screen_width = 0;
 screen_height = 0;
 apple = "";
 speak_data = "";
-to_number;
+to_number = "";
 draw_apple = "";
 
 var SpeechRecognition = window.webkitSpeechRecognition;
@@ -29,41 +29,7 @@ recognition.onresult = function(event) {
 
     document.getElementById("status").innerHTML = "The speech has been recognized: " + content; 
 
-}
-
-function setup() {
- 
-}
-
-function draw() {
-  if(draw_apple == "set")
-  {
-    document.getElementById("status").innerHTML = to_number + " Apples drawn";
-    draw_apple = "";
-  }
-
-   createCanvas(screen_width,screen_height-150);
-  
-  for(var i = 1; i <= to_number; i++){
-    x = Math.floor(Math.random()*700);
-    y = Math.floor(Math.random()*400);
-    image(apple, x, y, 50, 50);
-    document.getElementById("status").innerHTML = to_number +"Apples drawn";
-    speak();
-  }
-}
-
-function speak(){
-    var synth = window.speechSynthesis;
-
-    var utterThis = new SpeechSynthesisUtterance(speak_data);
-
-    synth.speak(utterThis);
-
-    speak_data = "";
-}
-
-to_number = Number(content);
+    to_number = Number(content);
 if(Number.isInteger(to_number)){
   document.getElementById("status").innerHTML = "Started  drawing apple";
   draw_apple = "set";
@@ -72,8 +38,41 @@ else{
   document.getElementById("status").innerHTML = "The speech has not recognized a number";
 }
 
-screen_width = window.innerWidth;
-screen_height = window.innerHeight;
+}
+
+function setup() {
+  screen_width = window.innerWidth;
+  screen_height = window.innerHeight;
+  
+  canvas = createCanvas(screen_width,screen_height-150);
+    canvas.position(0,150);
+}
+
+function draw() {
+  if(draw_apple == "set")
+  {
+    for(var i = 1; i <= to_number; i++){
+      x = Math.floor(Math.random()*700);
+      y = Math.floor(Math.random()*400);
+      image(apple, x, y, 50, 50);
+      
+    }
+    document.getElementById("status").innerHTML = to_number + " Apples drawn";
+    draw_apple = "";
+    speak_data = to_number + " Apples drawn";
+    speak();
+  }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    speak_data = "";
+}
 
 
-  canvas.position(0,150)
+
